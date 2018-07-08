d473d8e7ffd5a574e1977981a57b648abde4c84f
Stanley Huang <infamous_godhand@yahoo.com>
2018-07-08 10:17:30 +0000

    figured out it's the jsonp issue but unable to solve it

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios");
+var jsonp = require("jsonp");
+var fetchJsonp = require("fetch-jsonp");
+let jsonpAdapter = require('axios-jsonp');
+var getJSON = require('get-json');
+
+
 
 /* GET users listing. */
 router.get('/:api', function(req, res, next) {
   //res.send('respond with a resource');
   var abn = parseInt(req.params.api);
-  var URL = "https://abr.business.gov.au/json/AbnDetails.aspx?abn=" + abn + "&guid=b0c80e09-8d53-475e-819d-c0bce406385a";
+  var URL = "https://abr.business.gov.au/json/AbnDetails.aspx?abn=" + abn + "&callback=mycallback&guid=b0c80e09-8d53-475e-819d-c0bce406385a";
 
   axios.get(URL)
-    .then(function(response) {
-      // handle success
-
-      console.log(response.data);
-
+    .then(response => {
+      console.log(response.data)
     })
-    .catch(function(error) {
-      // handle error
-      console.log(error);
-    })
+    .catch(err => {
+      console.log(err)
+    });
 
-  //console.log(parseInt(req.params.api));
 
-  /*res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);*/
+
+
+
+
 });
 
 
