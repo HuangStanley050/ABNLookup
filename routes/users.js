d23bbead9eb8628b6b04fc7808d3f94434f8d592
Stanley Huang <infamous_godhand@yahoo.com>
2018-07-08 07:16:13 +0000

    run into issue fetching response from api call

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios");
 
 /* GET users listing. */
 router.get('/:api', function(req, res, next) {
   //res.send('respond with a resource');
-  console.log(parseInt(req.params.api) + 2);
+  var abn = parseInt(req.params.api);
+  var URL = "https://abr.business.gov.au/json/AbnDetails.aspx?abn=" + abn + "&guid=b0c80e09-8d53-475e-819d-c0bce406385a";
+
+  axios.get(URL)
+    .then(function(response) {
+      // handle success
+
+      console.log(response.data);
+
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+
+  //console.log(parseInt(req.params.api));
+
   /*res.json([{
   	id: 1,
   	username: "samsepi0l"
